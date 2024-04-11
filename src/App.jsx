3b689980c698a3cc7f35354d203e5629f90416b5
Nihal Limbu <7273236-NihalLimbu@users.noreply.replit.com>
2024-04-11 18:32:10 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+import Employees from './Employees';
+import Footer from './Footer';
+
+
+export default function App() {
+  return (
+    <div>
+      {/* React ⚛️ + Vite ⚡ + Replit */}
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </div>
+  )
+}
+
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// import "tailwindcss/base";
+// import "tailwindcss/components";
+// import "tailwindcss/utilities";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
