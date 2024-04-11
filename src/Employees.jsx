249abac9ead0ee0e4ead0f0f296363897eb1d674
NIHAL LIMBU <48156789+insane-arnold@users.noreply.github.com>
2024-04-11 19:43:29 +0000

    Employee card and select dropdown commit

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import maleProfile from './images/maleProfile.jpg';
-import feMaleProfile from './images/femaleProfile.jpg';
+import femaleProfile from './images/femaleProfile.jpg';
 
 const Employees = () => {
 
@@ -92,12 +92,35 @@ const [employees, setEmployees] = useState([
 ]);
   
   return (
-    <main>
-      {
-        employees.map((employee) => (
-          <p>{employee.fullName}</p>
-        ))    
-      }
+    <main className="container">
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-6">
+          <select className="form-select form-select-lg">
+            <option value="TeamA">Team A</option>
+            <option value="TeamB">Team B</option>
+            <option value="TeamC">Team C</option>
+            <option value="TeamD">Team D</option>
+          </select>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+          <div class="card-collection">
+            {
+              employees.map((employee)=>(
+                <div id={employee.id} className="card m-2" style={{cursor:"pointer"}}>
+                  <img src= {employee.gender==="male"? maleProfile:femaleProfile} className="card-img-top" />
+
+                  <div className='card-body'>
+                    <h5 className='card-title'>Full Name: {employee.fullName}</h5>
+                    <p className='card-text'><b>Designation:</b> {employee.designation}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </main>
   )
 
