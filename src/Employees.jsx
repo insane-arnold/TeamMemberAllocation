d1600a742ffbe5a27d2a15ef902ae6e2bdd722f2
NIHAL LIMBU <48156789+insane-arnold@users.noreply.github.com>
2024-04-13 17:58:19 +0000

    highlight team member commit

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,98 +4,111 @@ import femaleProfile from './images/femaleProfile.jpg';
 
 const Employees = () => {
 
-const [employees, setEmployees] = useState([
-  {
-    id: 1,
-    fullName: "Bob Jones",
-    designation: "JavaScript Developer",
-    gender: "male",
-    teamName: "TeamA"
-  },
-  {
-    id: 2,
-    fullName: "Jill Bailey",
-    designation: "Node Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 3,
-    fullName: "Gail Shepherd",
-    designation: "Java Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 4,
-    fullName: "Sam Reynolds",
-    designation: "React Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 5,
-    fullName: "David Henry",
-    designation: "DotNet Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 6,
-    fullName: "Sarah Blake",
-    designation: "SQL Server DBA",
-    gender: "female",
-    teamName: "TeamB"
-  },
-  {
-    id: 7,
-    fullName: "James Bennet",
-    designation: "Angular Developer",
-    gender: "male",
-    teamName: "TeamC"
-  },
-  {
-    id: 8,
-    fullName: "Jessica Faye",
-    designation: "API Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 9,
-    fullName: "Lita Stone",
-    designation: "C++ Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 10,
-    fullName: "Daniel Young",
-    designation: "Python Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 11,
-    fullName: "Adrian Jacobs",
-    designation: "Vue Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 12,
-    fullName: "Devin Monroe",
-    designation: "Graphic Designer",
-    gender: "male",
-    teamName: "TeamD"
+  const [selectedTeam, setTeam] = useState("TeamB");
+
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: "Bob Jones",
+      designation: "JavaScript Developer",
+      gender: "male",
+      teamName: "TeamA"
+    },
+    {
+      id: 2,
+      fullName: "Jill Bailey",
+      designation: "Node Developer",
+      gender: "female",
+      teamName: "TeamA"
+    },
+    {
+      id: 3,
+      fullName: "Gail Shepherd",
+      designation: "Java Developer",
+      gender: "female",
+      teamName: "TeamA"
+    },
+    {
+      id: 4,
+      fullName: "Sam Reynolds",
+      designation: "React Developer",
+      gender: "male",
+      teamName: "TeamB"
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB"
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB"
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC"
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC"
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC"
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD"
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD"
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD"
+    }
+  ]);
+
+  function handleTeanSelectionChange(event){
+    setTeam(event.target.value);
   }
-]);
+
+  function handleEmployeeCardClick(event){
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)?
+      (employee.teamName === selectedTeam)?{...employee, teamName:''}:{...employee, teamName:selectedTeam}:employee)
+
+    setEmployees(transformedEmployees);
+  }
   
   return (
     <main className="container">
-      <div class="row justify-content-center mt-3 mb-3">
-        <div class="col-6">
-          <select className="form-select form-select-lg">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-6">
+          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeanSelectionChange}>
             <option value="TeamA">Team A</option>
             <option value="TeamB">Team B</option>
             <option value="TeamC">Team C</option>
@@ -103,12 +116,13 @@ const [employees, setEmployees] = useState([
           </select>
         </div>
       </div>
-      <div class="row justify-content-center mt-3 mb-3">
-        <div class="col-8">
-          <div class="card-collection">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <div className="card-collection">
             {
               employees.map((employee)=>(
-                <div id={employee.id} className="card m-2" style={{cursor:"pointer"}}>
+                <div id={employee.id} className={(employee.teamName === selectedTeam)? "card m-2 standout" : "card m-2"} 
+                  style={{cursor:"pointer"}} onClick={handleEmployeeCardClick}>
                   <img src= {employee.gender==="male"? maleProfile:femaleProfile} className="card-img-top" />
 
                   <div className='card-body'>
